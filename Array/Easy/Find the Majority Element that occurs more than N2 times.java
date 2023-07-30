484742e41c4f693c8f6bd99a522239ad62d2dc79
Alaka singh <94798387+alakasingh@users.noreply.github.com>
2023-07-30 18:42:04 +0530

    Create Find the Majority Element that occurs more than N2 times.java

---
+++ Array/Easy/Find the Majority Element that occurs more than N2 times.java
@@ -0,0 +1,72 @@
+Find the Majority Element that occurs more than N/2 times
+Problem Statement: Given an array of N integers, write a program to return an element that occurs more than N/2 times in the given array. You may consider that such an element always exists in the array.
+
+Examples
+Example 1:
+Input Format: N = 3, nums[] = {3,2,3}
+Result: 3
+Explanation: When we just count the occurrences of each number and compare with half of the size of the array, you will get 3 for the above solution. 
+
+Example 2:
+Input Format:  N = 7, nums[] = {2,2,1,1,1,2,2}
+
+Result: 2
+
+Explanation: After counting the number of times each element appears and comparing it with half of array size, we get 2 as result.
+
+Example 3:
+Input Format:  N = 10, nums[] = {4,4,2,4,3,4,4,3,2,4}
+
+Result: 4
+
+  CODE STUDIO SOLUTION-------------------------------------------------------------------------------------------------------------------------------
+
+  import java.util.*;
+public class Solution {
+    public static int majorityElement(int []v) {
+        // Write your code here
+        int n = v.length;
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+
+        
+        for (int i = 0; i < n; i++) {
+            int value = mpp.getOrDefault(v[i], 0);
+            mpp.put(v[i], value + 1);
+        }
+
+        
+        for (Map.Entry<Integer, Integer> it : mpp.entrySet()) {
+            if (it.getValue() > (n / 2)) {
+                return it.getKey();
+            }
+        }
+
+        return -1;
+    }
+}
+
+LEETCODE SOLUTION----------------------------------------------------------------------------------------------------------------------------------------
+
+  class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+
+        //declaring a map:
+        HashMap<Integer, Integer> mpp = new HashMap<>();
+
+        //storing the elements with its occurnce:
+        for (int i = 0; i < n; i++) {
+            int value = mpp.getOrDefault(nums[i], 0);
+            mpp.put(nums[i], value + 1);
+        }
+
+        //searching for the majority element:
+        for (Map.Entry<Integer, Integer> it : mpp.entrySet()) {
+            if (it.getValue() > (n / 2)) {
+                return it.getKey();
+            }
+        }
+
+        return -1;
+    }
+}
