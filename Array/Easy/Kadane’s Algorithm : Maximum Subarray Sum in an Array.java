a70b33010d78773a5c0d3833ab82ae37110e8eab
Alaka singh <94798387+alakasingh@users.noreply.github.com>
2023-07-30 18:44:12 +0530

    Create Kadane’s Algorithm : Maximum Subarray Sum in an Array.java

---
+++ Array/Easy/Kadane’s Algorithm : Maximum Subarray Sum in an Array.java
@@ -0,0 +1,87 @@
+Kadane’s Algorithm : Maximum Subarray Sum in an Array
+Problem Statement: Given an integer array arr, find the contiguous subarray (containing at least one number) which
+has the largest sum and returns its sum and prints the subarray.
+
+Examples
+Example 1:
+
+Input: arr = [-2,1,-3,4,-1,2,1,-5,4] 
+
+Output: 6 
+
+Explanation: [4,-1,2,1] has the largest sum = 6. 
+
+Examples 2: 
+
+Input: arr = [1] 
+
+Output: 1 
+
+Explanation: Array has only one element and which is giving positive sum of 1. 
+
+  CODE STUDIO SOLUTION-----------------------------------------------------------------------------------------------------------------------------------
+
+  import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+	
+	public static long maxSubarraySum(int[] arr, int n) {
+		// write your code here
+		 long maxi = Long.MIN_VALUE; // maximum sum
+        long sum = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            sum += arr[i];
+
+            if (sum > maxi) {
+                maxi = sum;
+            }
+
+            // If sum < 0: discard the sum calculated
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        // To consider the sum of the empty subarray
+        // uncomment the following check:
+
+        //if (maxi < 0) maxi = 0;
+
+        return maxi;
+	}
+
+}
+
+LEETCODE SOLUTION---------------------------------------------------------------------------------------------------------------------------------------------
+
+  class Solution {
+    public int maxSubArray(int[] nums) {
+         int maxi = Integer.MIN_VALUE; // maximum sum
+         int sum = 0;
+         int n=nums.length;
+
+        for (int i = 0; i < n; i++) {
+
+            sum += nums[i];
+
+            if (sum > maxi) {
+                maxi = sum;
+            }
+
+            // If sum < 0: discard the sum calculated
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        // To consider the sum of the empty subarray
+        // uncomment the following check:
+
+        //if (maxi < 0) maxi = 0;
+
+        return maxi;
+    }
+}
