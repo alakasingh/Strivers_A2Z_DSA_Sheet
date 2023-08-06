25b8cedb123362044cb859fbd8bd1b8e4b570a91
Alaka singh <94798387+alakasingh@users.noreply.github.com>
2023-08-06 22:07:46 +0530

    Create Spiral Traversal of Matrix.java

---
+++ Array/Easy/Spiral Traversal of Matrix.java
@@ -0,0 +1,61 @@
+Spiral Traversal of Matrix
+Problem Statement: Given a Matrix, print the given matrix in spiral order.
+
+Examples:
+
+Example 1:
+Input: Matrix[][] = { { 1, 2, 3, 4 },
+		      { 5, 6, 7, 8 },
+		      { 9, 10, 11, 12 },
+	              { 13, 14, 15, 16 } }
+
+Outhput: 1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10.
+Explanation: The output of matrix in spiral form.
+
+CODE--------------------------------------------------------------------------------------------------------------------------------------
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] mat) {
+        List<Integer> ans = new ArrayList<>();
+        
+        int n = mat.length; // no. of rows
+        int m = mat[0].length; // no. of columns
+        
+        // Initialize the pointers required for traversal.
+        int top = 0, left = 0, bottom = n - 1, right = m - 1;
+
+        // Loop until all elements are not traversed.
+        while (top <= bottom && left <= right) {
+
+            // For moving left to right
+            for (int i = left; i <= right; i++)
+                ans.add(mat[top][i]);
+
+            top++;
+
+            // For moving top to bottom.
+            for (int i = top; i <= bottom; i++)
+                ans.add(mat[i][right]);
+
+            right--;
+
+            // For moving right to left.
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--)
+                    ans.add(mat[bottom][i]);
+
+                bottom--;
+            }
+
+            // For moving bottom to top.
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--)
+                    ans.add(mat[i][left]);
+
+                left++;
+            }
+        }
+         
+        return ans;
+    }
+}
