e17fcdfa4bf83a6506b88c227ed6e32eae005a11
Alaka singh <94798387+alakasingh@users.noreply.github.com>
2023-08-13 23:40:12 +0530

    Create Search Insert Position.java

---
+++ BINARY SEARCH/BS on 1D Arrays/Search Insert Position.java
@@ -0,0 +1,35 @@
+Search Insert Position
+Problem Statement: You are given a sorted array arr of distinct values and a target value x. You need to search for the index of the target value in the array.
+
+If the value is present in the array, then return its index. Otherwise, determine the index where it would be inserted in the array while maintaining the sorted order.
+
+Pre-requisite: Lower Bound & Binary Search
+
+Example 1:
+Input Format: arr[] = {1,2,4,7}, x = 6
+Result: 3
+Explanation: 6 is not present in the array. So, if we will insert 6 in the 3rd index(0-based indexing), the array will still be sorted. {1,2,4,6,7}.
+
+Example 2:
+Input Format: arr[] = {1,2,4,7}, x = 2
+Result: 1
+Explanation: 2 is present in the array and so we will return its index i.e. 1.
+
+code------------------------------------------------------------------------------------
+public class Solution {
+    public static int searchInsert(int [] arr, int m){
+        // Write your code here.
+        int low=0;
+        int high=arr.length-1;
+        int ans=arr.length;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]>=m){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }return ans;
+    }
+}
