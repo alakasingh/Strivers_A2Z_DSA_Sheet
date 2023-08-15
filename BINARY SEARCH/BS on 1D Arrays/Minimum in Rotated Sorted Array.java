ec755795791c723b7aa276c4f7f6c757da164483
Alaka singh <94798387+alakasingh@users.noreply.github.com>
2023-08-15 21:04:37 +0530

    Create Minimum in Rotated Sorted Array.java

---
+++ BINARY SEARCH/BS on 1D Arrays/Minimum in Rotated Sorted Array.java
@@ -0,0 +1,53 @@
+Minimum in Rotated Sorted Array
+  Problem Statement: Given an integer array arr of size N, sorted in ascending order (with distinct values). Now the array is rotated between 1 to N times which is unknown. Find the minimum element in the array. 
+
+Pre-requisites: Search in Rotated Sorted Array I,  Search in Rotated Sorted Array II & Binary Search algorithm
+
+Examples
+Example 1:
+Input Format: arr = [4,5,6,7,0,1,2,3]
+Result: 0
+Explanation: Here, the element 0 is the minimum element in the array.
+
+Example 2:
+Input Format: arr = [3,4,5,1,2]
+Result: 1
+Explanation: Here, the element 1 is the minimum element in the array.
+  code------------------------------------------------------------------
+
+public class Solution {
+    public static int findMin(int []arr) {
+        // Write your code here.
+        int low = 0, n=arr.length ,high = n - 1; 
+        int ans = Integer.MAX_VALUE;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            //search space is already sorted
+            //then arr[low] will always be
+            //the minimum in that search space:
+            if (arr[low] <= arr[high]) {
+                ans = Math.min(ans, arr[low]);
+                break;
+            }
+
+            //if left part is sorted:
+            if (arr[low] <= arr[mid]) {
+                // keep the minimum:
+                ans = Math.min(ans, arr[low]);
+
+                // Eliminate left half:
+                low = mid + 1;
+
+            } else { //if right part is sorted:
+
+                // keep the minimum:
+                ans = Math.min(ans, arr[mid]);
+
+                // Eliminate right half:
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
