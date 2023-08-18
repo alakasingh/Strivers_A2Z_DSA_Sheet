0ab70441245ce915137b52176ab8ca31aba1f710
Alaka singh <94798387+alakasingh@users.noreply.github.com>
2023-08-18 21:13:44 +0530

    Create Find the Smallest Divisor Given a Threshold.java

---
+++ BINARY SEARCH/BS ON ANSWER/Find the Smallest Divisor Given a Threshold.java
@@ -0,0 +1,51 @@
+Find the Smallest Divisor Given a Threshold
+Problem Statement: You are given an array of integers ‘arr’ and an integer i.e. a threshold value ‘limit’. Your task is to find the smallest positive integer divisor, such that upon dividing all the elements of the given array by it, the sum of the division’s result is less than or equal to the given threshold value.
+
+Examples
+Example 1:
+Input Format: N = 5, arr[] = {1,2,3,4,5}, limit = 8
+Result: 3
+Explanation: We can get a sum of 15(1 + 2 + 3 + 4 + 5) if we choose 1 as a divisor. 
+The sum is 9(1 + 1 + 2 + 2 + 3)  if we choose 2 as a divisor. Upon dividing all the elements of the array by 3, we get 1,1,1,2,2 respectively. Now, their sum is equal to 7 <= 8 i.e. the threshold value. So, 3 is the minimum possible answer.
+
+Example 2:
+Input Format: N = 4, arr[] = {8,4,2,3}, limit = 10
+Result: 2
+Explanation: If we choose 1, we get 17 as the sum. If we choose 2, we get 9(4+2+1+2) <= 10 as the answer. So, 2 is the answer.
+
+code-----------------------------------------------------------------------------------------------------------------------------------------------------------
+public class Solution {
+     public static int sumByD(int[] arr, int div) {
+        int n = arr.length; //size of array
+        //Find the summation of division values:
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += Math.ceil((double)(arr[i]) / (double)(div));
+        }
+        return sum;
+    }
+    public static int smallestDivisor(int arr[], int limit) {
+        // Write your coder here
+         int n = arr.length; //size of array.
+        if(n > limit) return -1;
+        //Find the maximum element:
+        int maxi = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            maxi = Math.max(maxi, arr[i]);
+        }
+
+        int low = 1, high = maxi;
+
+        //Apply binary search:
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (sumByD(arr, mid) <= limit) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+        
+    }
+}
