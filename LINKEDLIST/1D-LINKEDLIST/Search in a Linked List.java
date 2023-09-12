4bb3477206d62590775ad08e1d06483c08088be5
Alaka singh <94798387+alakasingh@users.noreply.github.com>
2023-09-12 20:49:17 +0530

    Create Search in a Linked List.java

---
+++ LINKEDLIST/1D-LINKEDLIST/Search in a Linked List.java
@@ -0,0 +1,48 @@
+Search in a Linked List
+  code--------------------------------
+ Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+    
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+     }
+    
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+     }
+    
+     Node(int data, Node next)
+     {
+         this.data = data;
+         this.next = next;
+     }
+ }
+
+ *****************************************************************/
+
+
+public class Solution
+{
+    public static int searchInLinkedList(Node head, int k)
+    {
+        // Write Your Code Here.
+        if(head.data==k){
+            return 1;
+        }
+        Node temp=head;
+        while(temp!=null){
+            if(temp.data==k){
+                return 1;
+            }
+            temp=temp.next;
+        }
+        return 0;
+    }
+} 
