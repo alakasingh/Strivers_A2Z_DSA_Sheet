6f3c218aa296d27be66f10eb8fed93100673ca3b
Alaka singh <94798387+alakasingh@users.noreply.github.com>
2023-09-13 20:06:30 +0530

    Create Introduction To Doubly Linked List.java

---
+++ LINKEDLIST/DOUBLY-LINKEDLIST/Introduction To Doubly Linked List.java
@@ -0,0 +1,55 @@
+Introduction To Doubly Linked List
+
+  code------------------------------------------
+   Following is the class structure of the Node class:
+
+ class Node {
+     public int data;
+     public Node next;
+     public Node prev;
+
+     Node()
+     {
+         this.data = 0;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data)
+     {
+         this.data = data;
+         this.next = null;
+         this.prev = null;
+     }
+
+     Node(int data, Node next, Node prev)
+     {
+         this.data = data;
+         this.next = next;
+         this.prev = prev;
+     }
+ };
+
+ *****************************************************************/
+
+public class Solution
+{
+    public static Node constructDLL(int []arr) {
+        // Write your code here
+        if (arr == null || arr.length == 0) {
+            return null;
+        }
+
+        Node head = new Node(arr[0]);
+        Node current = head;
+
+        for (int i = 1; i < arr.length; i++) {
+            Node newNode = new Node(arr[i]);
+            current.next = newNode;
+            newNode.prev = current;
+            current = newNode;
+        }
+
+        return head;
+    }
+}
