6676eaff14cfa4bd46588619773f2440b9937005
Alaka singh <94798387+alakasingh@users.noreply.github.com>
2023-09-18 20:10:20 +0530

    Create Selection Sort.java

---
+++ SORTING/Selection Sort.java
@@ -0,0 +1,25 @@
+Selection Sort
+
+  code---------------------------------------
+  public class Solution {
+    public static void selectionSort(int[] arr) {
+        //Your code goes here
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            // Find the index of the minimum element in the unsorted part of the array
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap the minimum element with the first element of the unsorted part
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+
+    }
+}
