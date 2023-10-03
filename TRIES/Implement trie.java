6b9be9d298161aff8a810cc781b7932986fd718b
Alaka singh <94798387+alakasingh@users.noreply.github.com>
2023-10-03 20:45:40 +0530

    Create Implement trie.java

---
+++ TRIES/Implement trie.java
@@ -0,0 +1,59 @@
+Implement trie
+
+  code---------------------------------------------------
+
+  class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    boolean isEnd;
+
+    TrieNode() {
+        this.isEnd = false;
+        for (int i = 0; i < 26; i++) {
+            children[i] = null;
+        }
+    }
+}
+
+class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new TrieNode();
+            }
+            node = node.children[index];
+        }
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                return false;
+            }
+            node = node.children[index];
+        }
+        return node != null && node.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = root;
+        for (char c : prefix.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null) {
+                return false;
+            }
+            node = node.children[index];
+        }
+        return true;
+    }
+}
